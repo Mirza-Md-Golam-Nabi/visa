64a8b47095cf28443b2ffc01ff2157d8f5466b2b
Nabi <g.nabi@shadhinbd.com>
2024-04-17 00:17:53 +0600

    Added marital status controller, model, migration, service, and seeder file

---
+++ database/seeders/MaritalStatusSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\MaritalStatus;
+use Illuminate\Database\Seeder;
+
+class MaritalStatusSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $marital_statuses = [
+            [
+                'title' => 'Married',
+            ],
+            [
+                'title' => 'Unmarried',
+            ],
+        ];
+
+        foreach ($marital_statuses as $marital_status) {
+            MaritalStatus::create($marital_status);
+        }
+    }
+}
